26bbef739e3f8b9e3cd2e14ad80111ea11a79978
Iris Diaz Feijoo <irisdf_4@hotmail.com>
2024-10-15 13:08:34 +0200

    Terminado ejercicio 5

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -2,6 +2,16 @@ package com.campusdual.classroom;
 
 public class Exercise05 {
     public static void main(String[] args) {
+        checkLessOrGreaterEqual(5,10);
+        checkLessOrGreaterEqual(10,10);
+        checkLessOrGreaterEqual(18,10);
+
+        checkMultiply(10,5);
+        checkMultiply(7,3);
+
+        checkPositiveNegative(-10);
+        checkPositiveNegative(7);
+        checkPositiveNegative(0);
 
     }
 
@@ -9,6 +19,11 @@ public class Exercise05 {
     // Si el valor es menor, tiene que devolver el siguiente mensaje: X es menor que Y
     // Si el valor es mayor o igual, tiene que devolver el siguiente mensaje: X es mayor o igual que Y
     public static void checkLessOrGreaterEqual(int n1, int n2) {
+        if (n1 < n2){
+            System.out.println(n1 + " es menor que " + n2);
+        } else if (n1 >= n2 ){
+            System.out.println(n1 + " es mayor o igual que " + n2);
+        }
 
     }
 
@@ -16,6 +31,12 @@ public class Exercise05 {
     // Si es múltiplo, tiene que devolver el siguiente mensaje: X es múltiplo de Y
     // Si no es múltiplo, tiene que devolver el siguiente mensaje: X no es múltiplo de Y
     public static void checkMultiply(int n1, int n2) {
+        if ( n1 % n2 == 0 ){
+            System.out.println(n1 + " es múltiplo de " + n2);
+        } else if ( n1 % n2 != 0) {
+            System.out.println(n1 + " no es múltiplo de " + n2);
+
+        }
 
     }
 
@@ -23,6 +44,11 @@ public class Exercise05 {
     // Si es negativo, tiene que devolver el siguiente mensaje: X es negativo.
     // Si es positivo o cero, tiene que devolver el siguiente mensaje: X es positivo.
     public static void checkPositiveNegative(int num) {
+        if (num < 0){
+            System.out.println( num + " es negativo ");
+        } else if ( num > 0){
+            System.out.println( num + " es positivo ");
+        }
         
     }
 }
